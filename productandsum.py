ba7761ef4e0977b7ee26417202a6d360208468a0
Franzjodie Domingo <franzjodiedomingo@gmail.com>
2024-01-26 19:15:45 +0800

    test

--- productandsum.py
+++ productandsum.py
@@ -1,4 +1,17 @@
 #Calculate the product and sum of two numbers
-#Using if else statement 
+#Using if else statement
 #Show the product if it is equal or greater than 1000, else show the sum
+
+def calculate(x, y):
+
+    product = x * y
+    if product <= 1000:
+        return product
+    else: 
+        return x + y
+    
 #Print the result
+num1 = 10
+num2 = 60
+result = calculate(num1, num2)
+print(f"The result is: {result}")
